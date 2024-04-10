0695df0414392492a971e89582bf86089882fa63
Alfred <alfredboahene.7@gmail.com>
2024-04-09 21:38:55 -0500

    wrote a mutliplication program using hack assembly language

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,37 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// my pseudo code:
+// from i = 1 to R1
+// add i to R2 (make sure R2 is 0)
+
+@i
+M=0
+
+@R2
+M=0
+
+(LOOP)
+@R1
+D=M
+
+@i
+D=M-D
+@END
+D;JEQ
+
+@R0
+D=M
+
+@R2
+M=M+D
+
+@i
+M=M+1
+
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
